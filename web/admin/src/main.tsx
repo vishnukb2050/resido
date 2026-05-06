effcbc2722f96ef69a89f1125c2b7e4a5379b617
vishnu <vishnukb2050@gmail.com>
2026-05-06 12:51:12 +0000

    Fix blank screen on admin panels by adding missing AuthProvider

--- web/admin/src/main.tsx
+++ web/admin/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
+import { AuthProvider } from './hooks/useAuth'
 import './index.css'
 
 const queryClient = new QueryClient()
@@ -10,9 +11,11 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
